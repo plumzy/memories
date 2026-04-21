1bbb6a9c356be200d5a406288ca9e3d81e09e0ed
plumzy <voidkaisa85@gmail.com>
2026-04-21 22:59:12 +0800

    Add service worker

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = "anniversary-memories-v1";
+const STATIC_ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.webmanifest",
+  "/assets/icon-192.svg",
+  "/assets/icon-512.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(STATIC_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith("/api/")) return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match("/index.html")))
+  );
+});
